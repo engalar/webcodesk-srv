77189fc0b47c777120a0db8b71a432697efa98c3
engalar <jason.liu@eingsoft.com>
2021-03-28 23:55:05 +0800

    单元测试示例

---
+++ src/usr/core/project/__tests__/projectResourcesUtils.js
@@ -0,0 +1,12 @@
+// import { updateResourceTree } from '../projectResourcesUtils';
+// import DeclarationsInFile from "../../parser/DeclarationsInFile";
+
+it('sums numbers', () => {
+    expect(3).toEqual(3);
+    expect(4).toEqual(4);
+});
+
+// it('updateResourceTree', () => {
+//     const df = new DeclarationsInFile("RESOURCE_IN_COMPONENTS_TYPE", [], "C:/Users/eg/repo/eingbuild-app1/node_modules/@emop/eingbuild-dep1/src/usr/atom/PageRouteAnchor.funcs.js")
+//     expect(updateResourceTree(df)).toEqual(4);
+// })
